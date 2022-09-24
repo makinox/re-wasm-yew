c9b0e8563cbd710fad397c5e53264264d482d754
Jesus Bossa <x@jesusbossa.dev>
2022-09-24 17:52:59 -0500

    chore: update web dependecies

--- webpack.config.js
+++ webpack.config.js
@@ -2,43 +2,42 @@ const path = require('path');
 const WasmPackPlugin = require('@wasm-tool/wasm-pack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-const distPath = path.resolve(__dirname, "dist");
-module.exports = (env, argv) => {
+const distPath = path.resolve(__dirname, 'dist');
+module.exports = (_, argv) => {
   return {
     devServer: {
-      contentBase: distPath,
+      static: {
+        directory: distPath,
+      },
       compress: argv.mode === 'production',
-      port: 8000
+      port: 8000,
     },
     entry: './bootstrap.js',
     output: {
       path: distPath,
-      filename: "todomvc.js",
-      webassemblyModuleFilename: "todomvc.wasm"
+      filename: 'todomvc.js',
+      webassemblyModuleFilename: 'todomvc.wasm',
     },
     module: {
       rules: [
         {
           test: /\.s[ac]ss$/i,
-          use: [
-            'style-loader',
-            'css-loader',
-            'sass-loader',
-          ],
+          use: ['style-loader', 'css-loader', 'sass-loader'],
         },
       ],
     },
     plugins: [
       new CopyWebpackPlugin({
-        patterns: [
-          { from: './static', to: distPath },
-        ],
+        patterns: [{ from: './static', to: distPath }],
       }),
       new WasmPackPlugin({
-        crateDirectory: ".",
-        extraArgs: "--no-typescript",
-      })
+        crateDirectory: '.',
+        extraArgs: '--no-typescript',
+      }),
     ],
-    watch: argv.mode !== 'production'
+    experiments: {
+      asyncWebAssembly: true,
+    },
+    watch: argv.mode !== 'production',
   };
 };
